db3fc8ce9fcc7b4e55f95fd051d4ff5654fb0123
Nitish <smartnitish15@gmail.com>
2020-06-04 00:05:24 +0530

    ADD | Implemented SVM over the results of PCA.

---
+++ PCA/PCA_SVM/svm_testing.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('../..')
+from sklearn.metrics import mean_squared_error
+import numpy as np
+import pickle
+import pandas as pd
+from accuracy_calculator import main as accuracy
+
+
+def testing():
+    X, y = testing_data_from_csv(filename_features="../MSFT_X_P_test.csv",
+                                 filename_variables="../MSFT_Y_P_test.csv")
+    X = X.values
+    y = y.values
+    test_svm(X, y)
+
+
+def testing_data_from_csv(filename_features, filename_variables):
+    X = pd.read_csv(filename_features, header=0)
+    y = pd.read_csv(filename_variables, header=0)
+    return X, y
+
+
+def test_svm(X_test, y_test):
+    file_name = 'svm_model.save'
+    model = pickle.load(open(file_name, 'rb'))
+    y_pred = model.predict(X_test)
+    length = np.size(y_pred)
+    mse_test = mean_squared_error(y_pred, y_test.ravel())
+    print("Mean Squared Error Result of Testing SVM")
+    print(mse_test)
+    accuracy_score = accuracy(y_pred, y_test.ravel(), length)
+    print('Accuracy Score: ', accuracy_score)
+
+
+if __name__ == "__main__":
+    testing()
---
+++ PCA/PCA_SVM/svm_training.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('../..')
+import pandas as pd
+import numpy as np
+import warnings
+
+
+def training():
+    X, y = training_data_from_csv(filename_features="../MSFT_X_P_learn.csv",
+                                  filename_variables="../MSFT_Y_P_learn.csv")
+    X = X.values
+    y = y.values
+    train_svm_model(X, y)
+
+
+def training_data_from_csv(filename_features, filename_variables):
+    """
+        Load csv file of features and target variables from csv
+        Args:
+            filename_features: string
+            filename_variables: string
+        Returns:
+            pandas.DataFrame
+    """
+    X = pd.read_csv(filename_features, header=0)
+    y = pd.read_csv(filename_variables, header=0)
+    return X, y
+
+
+def train_svm_model(X, y):
+    import svm_model as svm_scikit
+    svm_scikit.main(X, y)
+
+
+if __name__ == "__main__":
+    training()
