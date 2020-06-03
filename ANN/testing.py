48c92b981cadf6597ec0f39233219f9df5adf651
Nitish <smartnitish15@gmail.com>
2020-06-03 22:56:02 +0530

    Refactored | removed unused lines & changed csv filepaths to realtive paths.

--- ANN/testing.py
+++ ANN/testing.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,32 +9,30 @@ from matplotlib import cm
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import TimeSeriesSplit
 
+
 def testing():
-    filename="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT.csv"
-    X, y = testing_data_from_csv(filename_features="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_X_test.csv", 
-        filename_variables="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_y_test.csv")
-    X=X.values
-    print(y)
-    y=y.values
-    #print(y)
-    test_neural_net(X,y)
-    
+    X, y = testing_data_from_csv(filename_features="../MSFT_X_test.csv",
+                                 filename_variables="../MSFT_y_test.csv")
+    X = X.values
+    y = y.values
+    test_neural_net(X, y)
+
+
 def testing_data_from_csv(filename_features, filename_variables):
-        X=pd.read_csv(filename_features,header=0)
-        y=pd.read_csv(filename_variables,header=0)
-        return X,y
-        
-def test_neural_net(X_test,y_test):
-    X_test= PolynomialFeatures(degree=2).fit(X_test).transform(X_test)
-    file_name='finalized_model.sav'
-    model=pickle.load(open(file_name,'rb'))
-    #print(X_test.shape)
-    y_pred=model.predict(X_test)
-    #print(y_pred)
-    mse_test=mean_squared_error(y_pred,y_test)
+    X = pd.read_csv(filename_features, header=0)
+    y = pd.read_csv(filename_variables, header=0)
+    return X, y
+
+
+def test_neural_net(X_test, y_test):
+    X_test = PolynomialFeatures(degree=2).fit(X_test).transform(X_test)
+    file_name = 'finalized_model.sav'
+    model = pickle.load(open(file_name, 'rb'))
+    y_pred = model.predict(X_test)
+    mse_test = mean_squared_error(y_pred, y_test)
     print("Results obtained after testing")
     print(mse_test)
-    
+
+
 if __name__ == "__main__":
     testing()
-    
--- ANN/training.py
+++ ANN/training.py
@@ -1,27 +1,27 @@
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from matplotlib import cm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import warnings
+import sys
+sys.path.append('..')
 # Ignore warning: lib/python3.6/importlib/_bootstrap.py:219: RuntimeWarning: numpy.dtype size changed, may indicate binary incompatibility. Expected 96, got 88 return f(*args, **kwds)
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 
-from matplotlib import cm
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-from sklearn.model_selection import TimeSeriesSplit
 
 def training():
-    filename = "C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT.csv"
-    X, y = training_data_from_csv(filename_features="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_X_learn.csv", 
-        filename_variables="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_y_learn.csv")
-    X = X.values # convert to numpy.ndarray used by sklearn
-    y = y.values # convert to numpy.ndarray used by sklearn
-    print(y)
+    X, y = training_data_from_csv(filename_features="../MSFT_X_learn.csv",
+                                  filename_variables="../MSFT_y_learn.csv")
+    X = X.values  # convert to numpy.ndarray used by sklearn
+    y = y.values  # convert to numpy.ndarray used by sklearn
     tscv = TimeSeriesSplit(n_splits=10)
     train_neural_net(X, y, tscv, False)
- 
+
 
 def training_data_from_csv(filename_features, filename_variables):
     """
@@ -36,13 +36,16 @@ def training_data_from_csv(filename_features, filename_variables):
     y = pd.read_csv(filename_variables, header=0)
     return X, y
 
+
 def train_neural_net(X, y, tscv, grid_search=False):
     import neural_sklearn as nn_scikit
     nn_scikit.main(X, y, tscv.split(X))
 
+
 def performance(y_true, y_pred):
     mse = mean_squared_error(y_true, y_pred)
     return mse
+
 
 if __name__ == "__main__":
     training()
