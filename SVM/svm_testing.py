5155161b1ba6f39c45bc2802bd1c7863908eb2b4
Nitish <smartnitish15@gmail.com>
2020-06-03 23:53:10 +0530

    Refactored | removed unused lines & changed csv filepaths to relative paths.

--- SVM/svm_testing.py
+++ SVM/svm_testing.py
@@ -1,27 +1,32 @@
-import pandas as pd
-import pickle
 from sklearn.metrics import mean_squared_error
+import pickle
+import pandas as pd
+import sys
+sys.path.append('..')
+
 
 def testing():
-    filename="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT.csv"
-    X, y = testing_data_from_csv(filename_features="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_X_test.csv", 
-        filename_variables="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_y_test.csv")
-    X=X.values
-    y=y.values
-    test_svm(X,y)
-    
-def testing_data_from_csv(filename_features, filename_variables):
-        X=pd.read_csv(filename_features,header=0)
-        y=pd.read_csv(filename_variables,header=0)
-        return X,y
+    X, y = testing_data_from_csv(filename_features="../MSFT_X_test.csv",
+                                 filename_variables="../MSFT_y_test.csv")
+    X = X.values
+    y = y.values
+    test_svm(X, y)
 
-def test_svm(X_test,y_test):
-    file_name='svm_model.save'
-    model=pickle.load(open(file_name,'rb'))
-    y_pred=model.predict(X_test)
-    mse_test=mean_squared_error(y_pred,y_test.ravel())
+
+def testing_data_from_csv(filename_features, filename_variables):
+    X = pd.read_csv(filename_features, header=0)
+    y = pd.read_csv(filename_variables, header=0)
+    return X, y
+
+
+def test_svm(X_test, y_test):
+    file_name = 'svm_model.save'
+    model = pickle.load(open(file_name, 'rb'))
+    y_pred = model.predict(X_test)
+    mse_test = mean_squared_error(y_pred, y_test.ravel())
     print("Mean Squared Error Result of Testing SVM")
     print(mse_test)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     testing()
--- SVM/svm_training.py
+++ SVM/svm_training.py
@@ -1,22 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  9 23:12:25 2020
-
-@author: ASUS
-"""
-import warnings
-import numpy as np
 import pandas as pd
+import numpy as np
+import warnings
+import sys
+sys.path.append('..')
+
 
 def training():
-    filename = "C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT.csv"
-    X, y = training_data_from_csv(filename_features="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_X_learn.csv", 
-        filename_variables="C:\\Users\\ASUS\\Desktop\\StockMarket\\MSFT_y_learn.csv")
-    X=X.values
-    y=y.values
-    train_svm_model(X,y)
-    
-    
+    X, y = training_data_from_csv(filename_features="../MSFT_X_learn.csv",
+                                  filename_variables="../MSFT_y_learn.csv")
+    X = X.values
+    y = y.values
+    train_svm_model(X, y)
+
+
 def training_data_from_csv(filename_features, filename_variables):
     """
         Load csv file of features and target variables from csv
@@ -26,13 +22,15 @@ def training_data_from_csv(filename_features, filename_variables):
         Returns:
             pandas.DataFrame
     """
-    X=pd.read_csv(filename_features, header=0)
-    y=pd.read_csv(filename_variables, header=0)
-    return X,y
+    X = pd.read_csv(filename_features, header=0)
+    y = pd.read_csv(filename_variables, header=0)
+    return X, y
 
-def train_svm_model(X,y):
+
+def train_svm_model(X, y):
     import svm_model as svm_scikit
-    svm_scikit.main(X,y)
-    
-if __name__=="__main__":
+    svm_scikit.main(X, y)
+
+
+if __name__ == "__main__":
     training()
